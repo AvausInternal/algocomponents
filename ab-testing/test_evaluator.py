0a31a262e59d19f056ad49868e1e59e5d06ea5d7
Oskar Weinberger <Oskar.Weinberger@apotekhjartat.se>
2021-04-23 13:43:35 +0200

    first version of FW components

---
+++ ab-testing/test_evaluator.py
@@ -0,0 +1,111 @@
+from google.cloud import bigquery
+import pandas as pd
+from scipy import stats
+
+
+class TestEvaluator:
+    PROJECT = "algo-factory-dev"
+
+    def __init__(
+            self,
+            test_name
+    ):
+        self.test_name = test_name
+        self.client = bigquery.Client(project=self.PROJECT)
+        self.dataset = bigquery.Dataset('{PROJECT}.ab_test'.format(PROJECT=self.PROJECT))
+
+    def evaluate_test(self):
+        # Fetch metric sql query
+        df = self.client.query(
+            """
+                SELECT
+                    *
+                FROM {PROJECT}.ab_test.test_metrics t
+                INNER JOIN {PROJECT}.ab_test.metrics m
+                    ON t.metric_name = m.metric_name
+                WHERE t.test_name = '{test_name}'
+            """.format(PROJECT=self.PROJECT, test_name=self.test_name)
+        ).to_dataframe()
+        metric_name = df['metric_name'][0]
+        metric_query = df['sql'][0]
+
+        # Generate metric on customer level
+        customer_metric = self.client.query(metric_query).to_dataframe()
+
+        # Fetch customer key for the test
+        customer_key = self.client.query(
+            """
+                SELECT
+                    *
+                FROM {PROJECT}.ab_test.tests
+                WHERE test_name = '{test_name}'
+            """.format(PROJECT=self.PROJECT, test_name=self.test_name)
+        ).to_dataframe()['customer_key'][0]
+
+        # Fetch customer segments
+        segments = self.client.query(
+            """
+                SELECT
+                    group_name,
+                    customer_key AS {customer_key}
+                FROM {PROJECT}.ab_test.customer_segments
+                WHERE test_name = '{test_name}'
+            """.format(PROJECT=self.PROJECT, test_name=self.test_name, customer_key=customer_key)
+        ).to_dataframe()
+
+        # Join segment data to customer metrics table
+        joined_df = customer_metric.join(segments.set_index(customer_key),
+                                         on='{customer_key}'.format(customer_key=customer_key), how='inner')
+
+        # Calculate mean and standard deviation of metric for the segments
+        result_mean = joined_df.groupby('group_name')['metric'].mean()
+        result_std = joined_df.groupby('group_name')['metric'].std()
+
+        # Fetch groups data
+        groups_df = self.client.query(
+            """
+                SELECT
+                    group_name,
+                    is_control
+                FROM {PROJECT}.ab_test.groups
+                WHERE test_name = '{test_name}'
+            """.format(PROJECT='algo-factory-dev', test_name='Test 1')
+        ).to_dataframe()
+
+        pops = {}
+        for g in groups_df['group_name']:
+            pops[g] = list(joined_df.where(joined_df['group_name'] == g).dropna()['metric'])
+
+        control = groups_df.where(groups_df['is_control']).dropna()['group_name'].values[0]
+
+        test_results = pd.DataFrame()
+
+        for g in groups_df['group_name']:
+            t_score, p_value = stats.ttest_ind(pops[g], pops[control])
+            data = {
+                'group_name': g,
+                'metric_name': metric_name,
+                'metric_mean': result_mean[g],
+                'metric_std': result_std[g],
+                'significance': p_value
+            }
+            test_results = test_results.append(data, ignore_index=True)
+
+        # Prepend test name info
+        test_results.insert(loc=0, column='test_name', value=self.test_name)
+
+        self.write_to_table(test_results, 'test_results', overwrite=True)
+
+    def write_to_table(self, df, table_name, overwrite=False):
+        # Job config
+        if overwrite:
+            job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE")
+        else:
+            job_config = bigquery.LoadJobConfig()
+        job_config.autodetect = True
+        table = self.dataset.table(table_name)
+        job = self.client.load_table_from_dataframe(
+            dataframe=df, destination=table, job_config=job_config
+        )
+        job.result()
+
---
+++ ab-testing/test_group_splitter.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+
+class TestGroupSplitter:
+    def __init__(
+            self,
+            customer_df: pd.DataFrame,
+            customer_key: str,
+            fractions: tuple,
+            group_names: tuple
+    ):
+        self.customer_df = customer_df
+        self.customer_key = customer_key
+        self.fractions = fractions
+        self.group_names = group_names
+
+    def split(self):
+        if sum(self.fractions) != 1:
+            raise ValueError('Fractions must sum to 1!')
+        segments = self.customer_df[[self.customer_key]]
+        segments['randCol'] = np.random.rand(len(segments))
+        segments['group_name'] = segments['randCol']\
+            .apply(self._apply_split, fractions=self.fractions, group_names=self.group_names)
+        segments.rename(columns={self.customer_key: 'customer_key'}, inplace=True)
+        segments = segments[['group_name', 'customer_key']]
+        return segments
+
+    def _apply_split(self, x, fractions, group_names):
+        LB = 0
+        for i, f in enumerate(fractions):
+            UB = LB + f
+            if (x >= LB) & (x < (LB + f)):
+                return group_names[i]
+            LB = UB
