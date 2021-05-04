690f3d868dc8962846f128e274f19259ec2f83cb
Oskar Weinberger <oskar.weinberger@avaus.com>
2021-05-04 09:52:45 +0000

    add update and delete functionality

--- ab-testing/metric_creator.py
+++ ab-testing/metric_creator.py
@@ -34,11 +34,20 @@ class MetricCreator:
             }
         )
         self.write_to_table(
-            metric_entry, 
-            'metrics', 
-            'metric_name', 
-            self.metric_name, 
-            mode
+            df=metric_entry, 
+            table_name='metrics', 
+            key='metric_name', 
+            key_value=self.metric_name, 
+            mode=mode
+        )
+        
+    def delete_metric(self, mode='add'):
+        
+        # Remove from metrics table
+        self.delete_from_table(
+            table_name='metrics', 
+            key='metric_name', 
+            key_value=self.metric_name
         )
 
             
@@ -59,21 +68,10 @@ class MetricCreator:
             ).to_dataframe()
             if not check.empty:
                 raise ValueError(f'{key} with the value \'{key_value}\' already exists.')
-        elif mode == 'update' or mode == 'delete':
-            self.client.query(
-            """
-                DELETE FROM {PROJECT}.ab_test.{table_name}
-                WHERE {key} = '{key_value}'
-            """.format(
-                PROJECT=self.fw_params['PROJECT'],
-                table_name=table_name,
-                key=key,
-                key_value=key_value
-                )
-            )
-            if mode == 'delete': return
+        elif mode == 'update':
+            self.delete_from_table(table_name, key, key_value)
         else:
-            raise ValueError("The mode parameter can have the values \'add\', \'update\', or \'delete\'.")
+            raise ValueError("The mode parameter can have the values \'add\' or \'update\'.")
 
         # Job config
         job_config = bigquery.LoadJobConfig()
@@ -83,3 +81,16 @@ class MetricCreator:
             dataframe=df, destination=table, job_config=job_config
         )
         job.result()
+        
+    def delete_from_table(self, table_name, key, key_value):
+        self.client.query(
+        """
+            DELETE FROM {PROJECT}.ab_test.{table_name}
+            WHERE {key} = '{key_value}'
+        """.format(
+            PROJECT=self.fw_params['PROJECT'],
+            table_name=table_name,
+            key=key,
+            key_value=key_value
+            )
+        )
