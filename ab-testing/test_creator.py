0aed6c544155042724e74bea186569c6555782d6
Oskar Weinberger <oskar.weinberger@avaus.com>
2021-05-04 10:26:28 +0000

    move write and delete table functions to tools

--- ab-testing/test_creator.py
+++ ab-testing/test_creator.py
@@ -2,6 +2,7 @@ from google.cloud import bigquery
 import pandas as pd
 import yaml
 
+from tools.tools import write_to_table, delete_from_table
 from test_group_splitter import TestGroupSplitter
 
 
@@ -16,7 +17,6 @@ class TestCreator:
             self.fw_params = yaml.safe_load(config)
         
         self.client = bigquery.Client(project=self.fw_params['PROJECT'])
-        self.dataset = bigquery.Dataset('{PROJECT}.ab_test'.format(PROJECT=self.fw_params['PROJECT']))
         
         # Load test config parameters
         with open(test_config_path) as config:
@@ -33,8 +33,10 @@ class TestCreator:
                 'customer_key': [self.test_params['customer_key']]
             }
         )
-        self.write_to_table(
-            df=test_entry, 
+        write_to_table(
+            df=test_entry,
+            client=self.client,
+            project=self.fw_params['PROJECT'],
             table_name="tests", 
             key='test_name', 
             key_value=self.test_params['test_name'], 
@@ -50,8 +52,10 @@ class TestCreator:
                 'size': [g['size'] for g in self.test_params['groups']]
             }
         )
-        self.write_to_table(
-            df=groups_entry, 
+        write_to_table(
+            df=groups_entry,
+            client=self.client,
+            project=self.fw_params['PROJECT'],
             table_name="groups", 
             key='test_name', 
             key_value=self.test_params['test_name'], 
@@ -65,8 +69,10 @@ class TestCreator:
                 'metric_name': [self.test_params['metric_name']]
             }
         )
-        self.write_to_table(
-            df=metric_entry, 
+        write_to_table(
+            df=metric_entry,
+            client=self.client,
+            project=self.fw_params['PROJECT'],
             table_name="test_metrics", 
             key='test_name', 
             key_value=self.test_params['test_name'], 
@@ -100,8 +106,10 @@ class TestCreator:
         segments.insert(loc=0, column='test_name', value=self.test_params['test_name'])
 
         # Add group segments to customer_segments table
-        self.write_to_table(
-            df=segments, 
+        write_to_table(
+            df=segments,
+            client=self.client,
+            project=self.fw_params['PROJECT'],
             table_name="customer_segments",
             key='test_name', 
             key_value=self.test_params['test_name'], 
@@ -111,81 +119,46 @@ class TestCreator:
         
     def delete_test(self):
         # Remove from tests table
-        self.delete_from_table( 
+        delete_from_table(
+            client=self.client,
+            project=self.fw_params['PROJECT'],
             table_name="tests", 
             key='test_name', 
             key_value=self.test_params['test_name']
         )
         
         # Remove from groups table
-        self.delete_from_table(
+        delete_from_table(
+            client=self.client,
+            project=self.fw_params['PROJECT'],
             table_name="groups", 
             key='test_name', 
             key_value=self.test_params['test_name']
         )
         
         # Remove from test metrics table
-        self.delete_from_table(
+        delete_from_table(
+            client=self.client,
+            project=self.fw_params['PROJECT'],
             table_name="test_metrics", 
             key='test_name', 
             key_value=self.test_params['test_name']
         )
 
         # Remove from customer segments table
-        self.delete_from_table(
+        delete_from_table(
+            client=self.client,
+            project=self.fw_params['PROJECT'],
             table_name="customer_segments",
             key='test_name', 
             key_value=self.test_params['test_name']
         )
         
         # Remove from test results table
-        self.delete_from_table(
+        delete_from_table(
+            client=self.client,
+            project=self.fw_params['PROJECT'],
             table_name="test_results", 
             key='test_name', 
             key_value=self.test_params['test_name']
-        )
-        
-        
-    def write_to_table(self, df, table_name, key, key_value, mode):
-        if mode == 'add':
-            check = self.client.query(
-            """
-                SELECT
-                    {key}
-                FROM {PROJECT}.ab_test.{table_name}
-                WHERE {key} = '{key_value}'
-            """.format(
-                PROJECT=self.fw_params['PROJECT'],
-                table_name=table_name,
-                key=key,
-                key_value=key_value
-                )
-            ).to_dataframe()
-            if not check.empty:
-                raise ValueError(f'{key} with the value \'{key_value}\' already exists.')
-        elif mode == 'update':
-            self.delete_from_table(table_name, key, key_value)
-        else:
-            raise ValueError("The mode parameter can have the values \'add\' or \'update\'.")
-
-        # Job config
-        job_config = bigquery.LoadJobConfig()
-        job_config.autodetect = True
-        table = self.dataset.table(table_name)
-        job = self.client.load_table_from_dataframe(
-            dataframe=df, destination=table, job_config=job_config
-        )
-        job.result()
-        
-    def delete_from_table(self, table_name, key, key_value):
-        self.client.query(
-        """
-            DELETE FROM {PROJECT}.ab_test.{table_name}
-            WHERE {key} = '{key_value}'
-        """.format(
-            PROJECT=self.fw_params['PROJECT'],
-            table_name=table_name,
-            key=key,
-            key_value=key_value
-            )
         )
--- ab-testing/test_evaluator.py
+++ ab-testing/test_evaluator.py
@@ -3,6 +3,8 @@ import pandas as pd
 import yaml
 from scipy import stats
 
+from tools.tools import write_to_table, delete_from_table
+
 
 class TestEvaluator:
 
@@ -15,7 +17,6 @@ class TestEvaluator:
             self.fw_params = yaml.safe_load(config)
         
         self.client = bigquery.Client(project=self.fw_params['PROJECT'])
-        self.dataset = bigquery.Dataset('{PROJECT}.ab_test'.format(PROJECT=self.fw_params['PROJECT']))
         
         self.test_name = test_name
 
@@ -99,56 +100,12 @@ class TestEvaluator:
         # Prepend test name info
         test_results.insert(loc=0, column='test_name', value=self.test_name)
 
-        self.write_to_table(
-            df=test_results, 
+        write_to_table(
+            df=test_results,
+            client=self.client,
+            project=self.fw_params['PROJECT'],
             table_name="test_results", 
             key='test_name', 
             key_value=self.test_name, 
             mode=mode
         )
-        
-
-    def write_to_table(self, df, table_name, key, key_value, mode):
-        if mode == 'add':
-            check = self.client.query(
-            """
-                SELECT
-                    {key}
-                FROM {PROJECT}.ab_test.{table_name}
-                WHERE {key} = '{key_value}'
-            """.format(
-                PROJECT=self.fw_params['PROJECT'],
-                table_name=table_name,
-                key=key,
-                key_value=key_value
-                )
-            ).to_dataframe()
-            if not check.empty:
-                raise ValueError(f'{key} with the value \'{key_value}\' already exists.')
-        elif mode == 'update':
-            self.delete_from_table(table_name, key, key_value)
-        else:
-            raise ValueError("The mode parameter can have the values \'add\' or \'update\'.")
-
-        # Job config
-        job_config = bigquery.LoadJobConfig()
-        job_config.autodetect = True
-        table = self.dataset.table(table_name)
-        job = self.client.load_table_from_dataframe(
-            dataframe=df, destination=table, job_config=job_config
-        )
-        job.result()
-        
-    def delete_from_table(self, table_name, key, key_value):
-        self.client.query(
-        """
-            DELETE FROM {PROJECT}.ab_test.{table_name}
-            WHERE {key} = '{key_value}'
-        """.format(
-            PROJECT=self.fw_params['PROJECT'],
-            table_name=table_name,
-            key=key,
-            key_value=key_value
-            )
-        )
-
