bac28bc104769cbb0a0092078e1ace01198e5a31
Oskar Weinberger <oskar.weinberger@avaus.com>
2021-11-11 15:06:29 +0000

    group by to do the stratification

--- ab-testing/test_group_splitter.py
+++ ab-testing/test_group_splitter.py
@@ -24,7 +24,7 @@ class TestGroupSplitter:
         segments = self.customer_df[[self.customer_key] + self.strat_columns]
         segments['rand_col'] = np.random.rand(len(segments))
         if self.strat_columns:
-            segments['rank'] = self._get_ranking(segments)
+            segments['rank'] = segments.groupby(self.strat_columns)['rand_col'].rank(pct=True)
         else:        
             segments['rank'] = segments[self.customer_key].rank(pct=True)  
             
@@ -35,28 +35,6 @@ class TestGroupSplitter:
         segments.rename(columns={self.customer_key: 'customer_key'}, inplace=True)
         segments = segments[['group_name', 'customer_key']]
         return segments
-
-    def _get_ranking(self, segments):
-        unique_combinations = segments.groupby(self.strat_columns, as_index=False)[self.customer_key].nunique()
-        
-        def assign_rand(i,unique_combinations,segments):
-            rank = np.zeros(segments.shape[0])
-            idx = np.ones(segments.shape[0])
-            for j in range(len(self.strat_columns)):
-                cidx = segments[self.strat_columns[j]] == unique_combinations[self.strat_columns[j]][i]
-                idx = np.logical_and(idx,cidx)
-            values = np.linspace(0,1,unique_combinations[self.customer_key][i])
-            np.random.shuffle(values)
-            rank[idx] = values
-            return rank
-            
-        ranks = Parallel(n_jobs=-1, prefer='threads')(
-            delayed(assign_rand)(i,unique_combinations,segments) for i in range(unique_combinations.shape[0]))
-
-        ranks = np.asarray(ranks)
-        return ranks.sum(axis = 0)
-    
-
             
     def _get_boundaries(self, fractions, group_names):
         cutoff = 0
