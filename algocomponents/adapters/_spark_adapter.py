0d575efd9645240639d79638e07656454fdd6a22
Eric Hörberg <eric.horberg@avaus.se>
2022-05-02 14:01:44 +0200

    Add spark adapter

---
+++ algocomponents/adapters/_spark_adapter.py
@@ -0,0 +1,31 @@
+from configparser import ConfigParser
+
+from algocomponents.adapters import SQLAdapter
+
+
+class SparkAdapter(SQLAdapter):
+    """Used to run queries in spark.
+
+    This adapter functions when running queries in Databricks as well.
+    """
+
+    def __init__(self, config: ConfigParser = None):
+        super().__init__(overriding_config=config)
+        self.spark = None
+
+    def connect(self):
+        super().connect()
+        from pyspark.sql import SparkSession
+        self.spark = SparkSession.builder.getOrCreate()
+
+    def is_connected(self):
+        return self.spark is not None
+
+    def disconnect(self):
+        self.spark.stop()
+        self.spark = None
+        super().disconnect()
+
+    def _run_formatted_sql(self, sql: str):
+        sdf = self.spark.sql(sql)
+        sdf.show()
