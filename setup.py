7f63f887afda605e96c9f8c964b3e4a0cb4d46b7
Mateusz Utracki <mateusz.utracki@avaus.com>
2022-05-11 22:16:07 +0200

    Change licence and setup.py after Eric's review

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='algocomponents',
-    version='0.5.1',
+    version='0.5.0',
     packages=['algocomponents', 'algocomponents.tasks', 'algocomponents.utils', 'algocomponents.adapters'],
     url='',
     license='',
