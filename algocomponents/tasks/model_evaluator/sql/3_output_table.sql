326a6b636978e515f524d57d4e3b33f53334f720
Jonas <jonas.wikstrom@avaus.com>
2022-05-13 16:44:58 +0200

    Add model evaluator task

---
+++ algocomponents/tasks/model_evaluator/sql/3_output_table.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS {OUTPUT_TABLE};
+
+CREATE TABLE {OUTPUT_TABLE} AS
+SELECT
+    threshold,
+    TP,
+    FP,
+    TN,
+    FN,
+    ROUND(1.0*(TP + TN)/NULLIF((TP+TN+FP+FN),0),2) AS accuracy,
+    ROUND(1.0*TP/NULLIF(TP+FP,0),2) AS precision,
+    ROUND(1.0*TP/NULLIF(TP+FN,0),2) AS recall,
+    ROUND(2.0*TP/NULLIF(2*TP+FP+FN,0),2) AS f1_score
+FROM {MODEL_TMP_TABLE}
+ORDER BY threshold ASC
+;
