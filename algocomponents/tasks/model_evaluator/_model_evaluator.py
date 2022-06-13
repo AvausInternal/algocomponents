e5f39efeb1fa8b98c63250e0ec0231c086094d01
Eric Hörberg <eric.horberg@avaus.se>
2022-06-13 14:25:07 +0200

    Adapt queries to format table functionality

--- algocomponents/tasks/model_evaluator/_model_evaluator.py
+++ algocomponents/tasks/model_evaluator/_model_evaluator.py
@@ -1,5 +1,8 @@
+from configparser import ConfigParser
+
 from algocomponents.tasks import SQLPipeline
 from algocomponents.adapters import SQLAdapter
+
 
 class ModelEvaluator(SQLPipeline):
     """A task to evaluate models.
@@ -17,17 +20,21 @@ class ModelEvaluator(SQLPipeline):
             output_table: str,
             prediction_column: str,
             target_label_column: str,
-            sql_adapter: SQLAdapter= None,
+            sql_adapter: SQLAdapter = None,
+            config: ConfigParser = None,
+            section: str = None,
     ):
-        super().__init__(sql_adapter=sql_adapter)
-        
+        super().__init__(
+            sql_adapter=sql_adapter, config=config, section=section
+        )
+
         self.add_to_config("INPUT_TABLE", input_table)
         self.add_to_config("OUTPUT_TABLE", output_table)
         self.add_to_config("PREDICTION_COLUMN", prediction_column)
         self.add_to_config("TARGET_LABEL_COLUMN", target_label_column)
         self.add_to_config("THRESHOLD_STRING", self.create_thresholds(100))
     
-    def create_thresholds(self,n_thresholds):
+    def create_thresholds(self, n_thresholds):
         step = 100/n_thresholds
         s = f"SELECT {step/100} AS threshold"
         for i in range(2*int(step),100,int(step)):
---
+++ algocomponents/tasks/model_evaluator/sql/1_tmp_threshold_table.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS {TMP_DB}.threshold_table;
+
+CREATE TABLE {TMP_DB}.threshold_table AS
+{THRESHOLD_STRING}
+;
