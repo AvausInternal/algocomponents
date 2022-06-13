ef7a6e9ba3bc1ba0674c1af50e9cdfc6ee67f9ac
Eric Hörberg <eric.horberg@avaus.se>
2022-06-13 14:25:07 +0200

    Adapt queries to format table functionality

--- algocomponents/tasks/model_evaluator/sql/3_output_table.sql
+++ algocomponents/tasks/model_evaluator/sql/3_output_table.sql
@@ -11,6 +11,6 @@ SELECT
     ROUND(1.0*TP/NULLIF(TP+FP,0),2) AS precision,
     ROUND(1.0*TP/NULLIF(TP+FN,0),2) AS recall,
     ROUND(2.0*TP/NULLIF(2*TP+FP+FN,0),2) AS f1_score
-FROM {MODEL_TMP_TABLE}
+FROM {TMP_DB}.confusion_matrix
 ORDER BY threshold ASC
 ;
