5eac41cb16b3f31197d5af6ab972277436c734f6
Eric Hörberg <eric.horberg@avaus.se>
2022-06-14 21:29:30 +0200

    Add sql adapter propagation to GroupTask

--- algocomponents/tasks/_group_task.py
+++ algocomponents/tasks/_group_task.py
@@ -25,6 +25,9 @@ class GroupTask(AdapterTask):
         if not hasattr(self, "task_list"):
             self.task_list = []
 
+        if self.sql_adapter is not None:
+            self.propagate_sql_adapter(self.sql_adapter)
+
     def run(self):
         for task in self.task_list:
             task.parent = self
@@ -35,3 +38,13 @@ class GroupTask(AdapterTask):
 
         for task in self.task_list:
             task.add_to_config(key, value)
+
+    def set_sql_adapter(self, sql_adapter):
+        self.sql_adapter = sql_adapter
+        self.propagate_sql_adapter(sql_adapter)
+
+    def propagate_sql_adapter(self, sql_adapter):
+        for task in self.task_list:
+            if hasattr(task, "sql_adapter"):
+                if task.sql_adapter is None:
+                    task.set_sql_adapter(sql_adapter)
