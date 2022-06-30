34d48c2891ff7f2d1054e950fba991ba10102ce7
Eric Hörberg <eric.horberg@avaus.se>
2022-06-30 13:13:52 +0200

    Add missing folders in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@ from setuptools import setup
 setup(
     name='algocomponents',
     version='0.6.0',
-    packages=['algocomponents', 'algocomponents.tasks', 'algocomponents.utils', 'algocomponents.adapters'],
+    packages=[
+        'algocomponents',
+        'algocomponents.tasks',
+        'algocomponents.tasks.model_evaluator',
+        'algocomponents.tasks.model_evaluator.sql',
+        'algocomponents.utils',
+        'algocomponents.adapters',
+    ],
     url='',
     license='',
     author='erichorberg',
