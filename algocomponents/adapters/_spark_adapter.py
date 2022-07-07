6fd0b01b2e27bcdd5b4c55553b9597fe5204455b
Martti Yap <martti.yap@avaus.com>
2022-07-07 15:58:40 +0200

    Added table_exists and get_table_names methods for spark adapter

--- algocomponents/adapters/_spark_adapter.py
+++ algocomponents/adapters/_spark_adapter.py
@@ -30,6 +30,20 @@ class SparkAdapter(SQLAdapter):
     def _format_table_name(self, table: str):
         return table
 
+    def table_exists(self, database: str, table: str) -> bool:
+        """Tnis is divergent from all the other table_exists methods, in that it requires 2 inputs"""
+
+        sql_tables = self.spark.sql(f"SHOW TABLES in `{database}`").filter(
+            f"tableName = '{table}'"
+        )
+        return sql_tables.count() > 0
+
+    def get_table_columns(self, database: str, table: str) -> bool:
+        """Tnis is divergent from all the other get_table_columns methods, in that it requires 2 inputs"""
+
+        col_names = self.spark.sql(f"SHOW COLUMNS IN `{table}` IN `{database}`")
+        return col_names.rdd.map(lambda x: x[0]).collect()
+
     def _run_formatted_sql(self, sql: str):
         sdf = self.spark.sql(sql)
         sdf.show()
