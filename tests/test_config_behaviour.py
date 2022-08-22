50c6f0755ca082b06441d24287333b3a62efaf73
Eric Hörberg <eric.horberg@avaus.se>
2022-08-22 15:37:46 +0200

    Add config_inheritance_demo as unit test.

--- tests/test_config_behaviour.py
+++ tests/test_config_behaviour.py
@@ -4,6 +4,7 @@ from algocomponents.tasks import Task
 from configparser import ConfigParser
 
 
+# Need to define class here so that it reads the config/config.ini-file
 class EmptyTask(Task):
     pass
 
@@ -27,3 +28,42 @@ class TestConfigBehaviour(TestCase):
         assert task.config["DEFAULT"]["year"] == "1982"
         assert task.config["DEREZZED"]["name"] == "Tron Legacy"
         assert task.config["DEREZZED"]["year"] == "2010"
+
+    def test_that_add_to_config_has_highest_priority(self):
+        task = EmptyTask()
+        task.add_to_config("name", "TRON")
+        assert task.config["DEFAULT"]["name"] == "TRON"
+
+    def test_that_empty_sections_are_populated(self):
+        task = EmptyTask()
+        task.add_to_config("name", "TRON")
+        assert task.config["DEREZZED"]["name"] == "TRON"
+
+    def test_that_other_sections_are_not_overwritten(self):
+        config = ConfigParser()
+        config.add_section("DEREZZED")
+        config.set(section="DEREZZED", option="name", value="Tron Legacy")
+
+        task = EmptyTask(config=config)
+        task.add_to_config("name", "TRON")
+
+        assert task.config["DEFAULT"]["name"] == "TRON"
+        assert task.config["DEREZZED"]["name"] == "Tron Legacy"
+
+    def test_overwriting_to_inherited_value_works(self):
+        task = EmptyTask(section="DEREZZED")
+        task.add_to_config("name", "Tron Legacy")
+
+        assert task.config["DEFAULT"]["name"] == "Tron"
+        assert task.config["DEREZZED"]["name"] == "Tron Legacy"
+
+    def test_that_changed_section_is_overwritten(self):
+        config = ConfigParser()
+        config.add_section("DEREZZED")
+        config.set(section="DEREZZED", option="name", value="Tron Legacy")
+
+        task = EmptyTask(config=config, section="DEREZZED")
+        task.add_to_config("name", "TRON: Legacy")
+
+        assert task.config["DEFAULT"]["name"] == "Tron"
+        assert task.config["DEREZZED"]["name"] == "TRON: Legacy"
