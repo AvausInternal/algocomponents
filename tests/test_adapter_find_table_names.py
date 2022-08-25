9f4d4b01bfc2e1ddc6fb0b7472c03a66f2608869
Eric Hörberg <eric.horberg@avaus.se>
2022-08-25 11:45:22 +0200

    Add table finder test for EXTRACT() in gcp query

--- tests/test_adapter_find_table_names.py
+++ tests/test_adapter_find_table_names.py
@@ -1,12 +1,13 @@
 import random
 from unittest import TestCase
 
-from algocomponents.adapters import LocalSqliteAdapter
+from algocomponents.adapters import LocalSqliteAdapter, GCPAdapter
 
 
 class TestAdapterFindTableNames(TestCase):
 
     sql_adapter = LocalSqliteAdapter()
+    gcp_adapter = GCPAdapter()
 
     simple_query = """CREATE TABLE tmp"""
 
@@ -53,6 +54,14 @@ class TestAdapterFindTableNames(TestCase):
         CROSS JOIN avg
     """
 
+    query_with_gcp_extract_method = """
+        CREATE TABLE other_client_db.customer_product_sales_table AS
+        SELECT
+            a.customer_id,
+            EXTRACT(ISOWEEK FROM start_at) as iso_week
+        FROM client_db.sales_table a
+    """
+
     drop_table_statement = """
         DROP TABLE client_db.customer_product_table
     """
@@ -77,6 +86,11 @@ class TestAdapterFindTableNames(TestCase):
         "gcp-project.client_db.sales_table",
         "gcp-project.other_client_db.customer_product_sales_table",
     ]
+    query_with_gcp_extract_tables = [
+        "client_db.sales_table",
+        "other_client_db.customer_product_sales_table",
+        # Notably, "start_at" should not be a table even though it trails FROM
+    ]
 
     def test_finding_single_table(self):
         table_names = self.sql_adapter.find_table_names(sql=self.simple_query)
@@ -94,6 +108,32 @@ class TestAdapterFindTableNames(TestCase):
         table_names = [t.lower() for t in table_names]
 
         assert table_names == self.simple_tables
+
+    def test_finding_tables_without_finding_parameters_in_gcp_method(self):
+        table_names = self.gcp_adapter.find_table_names(
+            sql=self.query_with_gcp_extract_method
+        )
+
+        assert sorted(table_names) == self.query_with_gcp_extract_tables
+
+    def test_finding_tables_without_finding_parameters_in_gcp_method_bad_formatting(
+        self,
+    ):
+        query_with_gcp_extract_method_bad_formatting = self.sql_query_format_scrambler(
+            self.query_with_gcp_extract_method
+        )
+
+        table_names = self.gcp_adapter.find_table_names(
+            sql=query_with_gcp_extract_method_bad_formatting
+        )
+
+        # Lowercase necessary as query formatting is scrambled
+        table_names = [t.lower() for t in table_names]
+
+        # Check that every table is found
+        assert all([t in table_names for t in self.query_with_gcp_extract_tables])
+        # Check that only expected tables are found
+        assert all([t in self.query_with_gcp_extract_tables for t in table_names])
 
     def test_finding_multiple_tables(self):
         table_names = self.sql_adapter.find_table_names(sql=self.advanced_query)
