c5bc49767524a8797a7b57455ce81bfeafc61730
Eric Hörberg <eric.horberg@avaus.se>
2022-08-31 23:54:12 +0200

    Add test with complicated gcp query
    The query includes usages of unnest and multiple ctes.

--- tests/test_gcp_adapter_format_tables.py
+++ tests/test_gcp_adapter_format_tables.py
@@ -140,6 +140,42 @@ class TestGCPAdapterFormatTables(TestCase):
             AND a.product_id = b.product_id
         CROSS JOIN avg
     """
+    advanced_query_with_wildcard_table = """
+        WITH sessions_events as(
+          SELECT
+            (SELECT value.string_value FROM UNNEST(event_params) where key = "page_location") AS locations,
+            (SELECT value.int_value FROM UNNEST(event_params) where key = "ga_session_id") AS ga_session_id,
+            MAX(user_pseudo_id) AS user_pseudo_id,
+            MAX(Case event_name when 'session_start' then event_timestamp end) session_start,
+            MAX(Case event_name when 'user_engagement' then event_timestamp  end) end_visit,
+            MAX(event_date) AS event_date,
+            MAX((SELECT value.string_value FROM UNNEST(event_params) where key = "page_title")) AS title,
+            MAX((SELECT value.string_value FROM UNNEST(event_params) where key = "page_referrer")) AS referrer, 
+            MAX((SELECT value.string_value FROM UNNEST(event_params) where key = "Article tag")) AS article_tag,
+          FROM `avaus-com-ga.analytics_243066661.events_*` 
+          GROUP BY ga_session_id, locations
+        ),
+          sessions_duration as( 
+          SELECT user_pseudo_id, 
+          PARSE_DATE('%Y%m%d', event_date) AS event_date,
+          SAFE_SUBTRACT(end_visit, session_start) as duration,
+          ga_session_id,
+          article_tag,
+          STRUCT(title, locations, referrer) AS page
+          FROM sessions_events
+          order by ga_session_id
+          
+        )
+        
+          SELECT user_pseudo_id, event_date,
+          duration,
+          ga_session_id,
+          article_tag,
+          page
+          FROM sessions_duration
+          WHERE event_date BETWEEN '2021-08-01' AND CURRENT_DATE()
+          order by event_date
+    """
 
     def test_formatting_simple_query(self):
         query = self.gcp_adapter._format_table_names(query=self.simple_query)
@@ -214,3 +250,9 @@ class TestGCPAdapterFormatTables(TestCase):
             query=self.advanced_query_with_cte_formatted
         )
         assert query == self.advanced_query_with_cte_formatted
+
+    def test_formatting_advanced_query_with_wildcard_table(self):
+        query = self.gcp_adapter_without_config._format_table_names(
+            query=self.advanced_query_with_wildcard_table
+        )
+        assert query == self.advanced_query_with_wildcard_table
