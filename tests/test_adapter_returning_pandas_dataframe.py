0959dceea4e8901afbdf3985b5fe81d9e2adb76b
Eric Hörberg <eric.horberg@avaus.se>
2022-09-06 13:14:04 +0200

    Always return entire df, config only changes logs

--- tests/test_adapter_returning_pandas_dataframe.py
+++ tests/test_adapter_returning_pandas_dataframe.py
@@ -16,14 +16,7 @@ class EmptyTask(Task):
 
 class TestAdapterReturningPandasDataframe(TestCase):
 
-    row_limit = 1
-    max_one_row_config = ConfigParser()
-    max_one_row_config.set(
-        section="DEFAULT", option="max_rows_returned", value=str(row_limit)
-    )
-
     sql_adapter = LocalSqliteAdapter()
-    sql_adapter_max_one_row = LocalSqliteAdapter(config=max_one_row_config)
 
     def test_that_list_of_pandas_dataframe_is_returned(self):
         self.sql_adapter.connect()
@@ -48,18 +41,27 @@ class TestAdapterReturningPandasDataframe(TestCase):
         df_list = self.sql_adapter.run_sql_file(
             os.path.join(sql_folder, "large_query.sql")
         )
-        assert df_list[0].shape[0] == self.sql_adapter.default_max_rows_returned
+        assert df_list[0].shape[0] == 30
         self.sql_adapter.disconnect()
 
-    def test_that_pandas_dataframe_size_adjusts_with_config(self):
-        self.sql_adapter_max_one_row.connect()
+    def test_that_adapters_pick_up_max_rows_displayed_config(self):
+        row_limit = 1
+        max_one_row_config = ConfigParser()
+        max_one_row_config.set(
+            section="DEFAULT", option="max_rows_displayed", value=str(row_limit)
+        )
+        sql_adapter_max_one_row = LocalSqliteAdapter(config=max_one_row_config)
+        assert sql_adapter_max_one_row.max_rows_displayed == row_limit
+
+        # Should not affect shape of returned dataframe
+        sql_adapter_max_one_row.connect()
         module = sys.modules[self.__class__.__module__]
         sql_folder = os.path.join(
             os.path.dirname(module.__file__),
             "test_adapter_returning_pandas_dataframe_queries",
         )
-        df_list = self.sql_adapter_max_one_row.run_sql_file(
+        df_list = sql_adapter_max_one_row.run_sql_file(
             os.path.join(sql_folder, "large_query.sql")
         )
-        assert df_list[0].shape[0] == self.row_limit
-        self.sql_adapter_max_one_row.disconnect()
+        assert df_list[0].shape[0] == 30
+        sql_adapter_max_one_row.disconnect()
