0d56e4ac6f6a497aa7bcdea293b25f88ab6a6041
karolina.meyer <karolina.meyer@avaus.com>
2022-09-19 17:29:38 +0200

    Add VisualizeDataset that saves plots from tables.

---
+++ algocomponents/utils/_save_plots.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def save_boxplot(
+        df: pd.DataFrame,
+        output_folder: str = "",
+        file_name: str = "boxplot"
+):
+    fig = plt.figure(figsize=(10, 7))
+    sns.boxplot(data=df)
+
+    if output_folder != "":
+        output_folder = output_folder + "/"
+
+    plt.savefig(f"{output_folder}{file_name}.png")
+    print(f"Boxplot saved under \'{file_name}.png\' file")
+
+def save_histogram(
+        df: pd.DataFrame,
+        output_folder: str = "",
+        file_name: str = "histogram"
+):
+    fig = plt.figure(figsize=(10, 7))
+    cols = df.columns
+    for col in cols:
+        sns.histplot(data=df, x=col)
+        col_name = col.replace(" ", "_")
+        plt.savefig(f"{output_folder}{file_name}-{col_name}.png")
+        plt.clf()
+        print(f"Histogram of a feature: \'{col}\' saved under \'{file_name}-{col_name}.png\' file")
