e37e2d77e6ce21681bf43ca03ee41d02348eea4d
Mateusz Utracki <mateusz.utracki@avaus.com>
2022-10-11 08:19:20 +0200

    Add unit test for GCP adapter new keywords

---
+++ tests/adapters/gcp_adapter_format_queries/query_like_table.sql
@@ -0,0 +1,2 @@
+CREATE SNAPSHOT TABLE  weather_data.test123 CLONE weather_data.weather_forecast_Helsinki;
+CREATE SNAPSHOT TABLE  `avaus-academy.weather_data.test123` CLONE `avaus-academy.weather_data.weather_forecast_Helsinki`
