9ecf3b89cd159603be56c84365c04283d20631f3
Black Formatter <runner@fv-az489-837.ommslwehxtse5c44z5cafbrg5d.jx.internal.cloudapp.net>
2022-11-11 06:48:57 +0000

    Black formatting

--- algocomponents/tasks/feature_stratify_groups/main.py
+++ algocomponents/tasks/feature_stratify_groups/main.py
@@ -3,10 +3,11 @@ from pipe_line.Stratify_Groups import Stratify_Groups
 
 sql_pipeline = Stratify_Groups(
     sql_adapter=GCPAdapter(),
-    input_table='Customer',
+    input_table="Customer",
     primary_keys=["CustomerId"],
     stratify_on="CustomerId, Country, City",
     nbr_groups=4,
-    output_table='Stratified')
+    output_table="Stratified",
+)
 
 sql_pipeline.start()
--- algocomponents/tasks/feature_stratify_groups/pipe_line/Stratify_Groups.py
+++ algocomponents/tasks/feature_stratify_groups/pipe_line/Stratify_Groups.py
@@ -3,18 +3,18 @@ from algocomponents.adapters import SQLAdapter
 
 
 class Stratify_Groups(SQLPipeline):
-    """ Given a table, a set of columns that decide the group, 
-    what columns to stratify on and how many groups, 
-    an output table is created with stratified groups. """
+    """Given a table, a set of columns that decide the group,
+    what columns to stratify on and how many groups,
+    an output table is created with stratified groups."""
 
     def __init__(
-            self,
-            sql_adapter: SQLAdapter,
-            input_table: str,  # CUSTOMER_DB.all_profilable_customers
-            primary_keys: list,  # ["customer_key"]
-            stratify_on: str,  # "age", postal_code"
-            nbr_groups: int,  # 2
-            output_table: str,  # AB_TESTING_DB.my_test_groups
+        self,
+        sql_adapter: SQLAdapter,
+        input_table: str,  # CUSTOMER_DB.all_profilable_customers
+        primary_keys: list,  # ["customer_key"]
+        stratify_on: str,  # "age", postal_code"
+        nbr_groups: int,  # 2
+        output_table: str,  # AB_TESTING_DB.my_test_groups
     ):
         super().__init__(sql_adapter=sql_adapter)
         self.add_to_config("INPUT_TABLE", input_table)
