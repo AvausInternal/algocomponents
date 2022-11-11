bb6d94c0a7163073a49edf2a2df883d2f32dd33a
Eric Hörberg <eric.horberg@avaus.se>
2022-11-11 11:10:37 +0100

    Update to version 0.8.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="algocomponents",
-    version="0.7.0",
+    version="0.8.0",
     packages=[
         "algocomponents",
         "algocomponents.tasks",
