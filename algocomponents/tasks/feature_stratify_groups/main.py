baf3f060512ccd49798575e04e724b93cc75e268
Naeim <naeim.rashidfarokhi@avaus.com>
2022-11-11 08:37:37 +0100

    Black formatting

--- algocomponents/tasks/feature_stratify_groups/main.py
+++ algocomponents/tasks/feature_stratify_groups/main.py
@@ -1,12 +1,12 @@
 from algocomponents.adapters import GCPAdapter
-from pipe_line.Stratify_Groups import Stratify_Groups
+from pipe_line.stratifyGroups import StratifyGroups
 
-sql_pipeline = Stratify_Groups(
+sql_pipeline = StratifyGroups(
     sql_adapter=GCPAdapter(),
-    input_table='Customer',
-    primary_keys=["CustomerId"],
+    input_table="Customer",
     stratify_on="CustomerId, Country, City",
-    nbr_groups=4,
-    output_table='Stratified')
+    nbr_groups=3,
+    output_table="Stratified",
+)
 
 sql_pipeline.start()
