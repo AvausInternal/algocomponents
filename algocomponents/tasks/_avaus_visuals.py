5f31cd9b60fce4e3bedca1857e63cfab88b476c7
Marianne Clement <marianneclement@Marianne.local>
2022-11-17 10:05:43 +0100

    changes for scatterplot

--- algocomponents/tasks/_avaus_visuals.py
+++ algocomponents/tasks/_avaus_visuals.py
@@ -3,6 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from typing import List
+import urllib.request
+from urllib.request import urlopen
+
+
+# TODO:
+"""
+-
+violine plot
+subplot
+color palette 
+sns.violinplot(data=df, x="deck", y="age", hue="alive", split=True)
+"""
 
 
 class AvausVisuals:
@@ -24,7 +36,18 @@ class AvausVisuals:
         "WOOD": "#DEC8C0",
     }
     color_list = ["#363760", "#135A61", "#FAEFED", "#515458"]
+    scatterplot_color =  [
+        primary_colors["BLUEBERRY"],
+        secondary_colors["SKY"], 
+        secondary_colors["CLOUDBERRY"],
+        primary_colors["PINE"]
+    ]
+    barplot_color = sns.set_palette(sns.color_palette( ["#363760", "#135A61"]))
     heatmap_color = sns.light_palette("#363760", input="rgb", as_cmap=True)
+    color = ["#135A61"]
+    lineplot_color = sns.set_palette(sns.color_palette(color))
+    Color2 =  ["#363760", "#135A61"]
+    histo_color = sns.set_palette(sns.color_palette(Color2))
 
     def __init__(self):
         sns.set_style("whitegrid", {"axes.grid": False})
@@ -38,30 +61,35 @@ class AvausVisuals:
         mono_color: bool = True,
         x_col: str = None,
         y_cols: List[str] = None,
+        legend: bool = False,
     ):
 
         if x_col and y_cols:
-            if mono_color:
-                df_multi_transform = df_multi.melt(
+            df_multi_transform = df_multi.melt(
                     x_col, var_name="Year", value_name="Sales"
                 )
+            if mono_color:
+                
                 sns.barplot(
                     data=df_multi_transform,
                     x=x_col,
                     y="Sales",
                     hue="Year",
-                    color=self.color_list[0],
+                   color=self. color_list[0],
+                   palette=self. color_list
                 )
             else:
-                sns.barplot(data=df, x=x_col, y=y_cols, palette=self.color_list)
+
+                sns.barplot(data=df, x=x_col, y=y_cols,  palette=self.color)
         else:
             if mono_color:
-                sns.barplot(data=df, color=self.color_list[0])
+                sns.barplot(data=df, color=self. color_list[0])
             else:
-                sns.barplot(data=df, palette=self.color_list)
+
+                sns.barplot(data=df, palette=self.barplot_color)
 
         self.format(
-            df=df, title=title, file_name=file_name, output_folder=output_folder
+            df=df, title=title, file_name=file_name, output_folder=output_folder, legend = legend 
         )
 
     def lineplot(
@@ -74,7 +102,10 @@ class AvausVisuals:
         y_cols: List[str] = None,
     ):
 
-        sns.lineplot(data=df, x=x_col, y=y_cols, palette=self.color_list, dashes=False)
+        sns.lineplot(
+            data=df, x=x_col, y=y_cols, palette=self.lineplot_color, dashes=False
+        )
+
         self.format(
             df=df, title=title, file_name=file_name, output_folder=output_folder
         )
@@ -87,9 +118,23 @@ class AvausVisuals:
         output_folder: str = None,
         x_col: str = None,
         y_cols: List[str] = None,
-        hue: str = None,
+        legend: bool = False,
     ):
-        sns.histplot(data=df, x=x_col, y=y_cols, palette=self.color_list)
+        sns.histplot(data=df, x=x_col, y=y_cols, palette =self.histo_color)
+        self.format( 
+            df=df, title=title, file_name=file_name, output_folder=output_folder, legend=legend
+        )
+
+    def heatmap(
+        self,
+        df: pd.DataFrame,
+        title: str = None,
+        file_name: str = None,
+        x_col: str = None,
+        y_cols: str = None,
+        output_folder: str = None,
+    ):
+        sns.heatmap(data=df, cmap=self.heatmap_color)
         self.format(
             df=df, title=title, file_name=file_name, output_folder=output_folder
         )
@@ -102,23 +147,37 @@ class AvausVisuals:
         x_col: str = None,
         y_cols: str = None,
         output_folder: str = None,
+        legend: bool = False,
+        
+       
     ):
-        sns.scatterplot(data=df, palette=self.color_list)
+        n_colors = len(df.columns)
+        palette = self.scatterplot_color[:n_colors]
+        sns.scatterplot(data=df,x=x_col, y=y_cols, palette= palette ,  s= 200)
         self.format(
-            df=df, title=title, file_name=file_name, output_folder=output_folder
+            df=df, title=title, file_name=file_name, output_folder=output_folder, legend=legend ,  
         )
 
-    def heatmap(
+    def violinplot(
         self,
         df: pd.DataFrame,
-        annot: True,
         title: str = None,
         file_name: str = None,
+        x_col: str = None,
+        y_cols: str = None,
+        hue: str = None,
         output_folder: str = None,
+        legend: bool = False,
     ):
-        sns.heatmap(data=df, cmap=self.heatmap_color)
+        sns.violinplot(
+            data=df, x=x_col, y=y_cols, hue=hue, palette=self.color_list, split=True
+        )
         self.format(
-            df=df, title=title, file_name=file_name, output_folder=output_folder
+            df=df,
+            title=title,
+            file_name=file_name,
+            output_folder=output_folder,
+            legend=legend,
         )
 
     def pimp_my_plot(self):
@@ -128,6 +187,7 @@ class AvausVisuals:
         self,
         df: pd.DataFrame,
         title: str = None,
+        legend: bool = False,
         file_name: str = None,
         output_folder: str = None,
     ):
@@ -143,10 +203,21 @@ class AvausVisuals:
         plt.ylabel("")
         plt.grid(axis="y")
 
-        # TODO Check if data contain negative numbers and set axis acordingly (Draft)
-        numeric = df.select_dtypes(include=np.number)
-        if min(numeric.min()) > 0:
-            plt.ylim(bottom=0)
+        # legend
+
+        if legend:
+            plt.legend(
+                loc="upper center",
+                bbox_to_anchor=(0.7, 1.10),
+                fancybox=True,
+                shadow=True,
+                ncol=5,
+            )
+
+        # TODO Check if y-columns contain negative numbers
+        # num = df.select_dtypes(include=np.number)
+        # if (num["Sales2021"] > 0).any():
+        # plt.ylim(bottom=0)
 
         # Save plot if path and name is defined
         if output_folder and file_name:
@@ -161,7 +232,7 @@ if __name__ == "__main__":
 
     df_ab_test = pd.DataFrame([[0.10, 0.22]], columns=["Test", "Control"])
 
-    # df_monthly_sales = pd.DataFrame([[132, 232, 254, 343, 154, 222, 254, 343, 254, 323, 432, 267]], columns=["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"])
+    df_monthly_sales = pd.DataFrame([[132, 232, 254, 343, 154, 222, 254, 343, 254, 323, 432, 267]], columns=["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"])
     df_sales_col = pd.DataFrame(
         [
             ["Jan", 12],
@@ -204,19 +275,21 @@ if __name__ == "__main__":
     Index = ["aaa", "bbb", "ccc", "ddd", "eee"]
     Cols = ["A", "B", "C", "D"]
     df_heatmap = pd.DataFrame(abs(np.random.randn(5, 4)), index=Index, columns=Cols)
-
     df_scatter = pd.DataFrame(
-        [[5.1, 3.5, 0], [4.9, 3.0, 0], [7.0, 3.2, 1], [6.4, 3.2, 1], [5.9, 3.0, 2]],
-        columns=["length", "width", "species"],
+        [[5.1, 3.5, 0,9], [4.9, 3.0, 0, 3], [7.0, 3.2, 1, 2], [6.4, 3.2, 1, 2], [5.9, 3.0, 2, 5]],
+        columns=["length", "width", "species", "test"],
     )
+  #print (df_scatter)
+plotter.barplot (df= df_monthly_sales,title= "Barplot", )
 
-    # plotter.histplot(df=df_multi2, title="hej")
-    plotter.scatterplot(df=df_scatter)
-    # plotter.barplot(df=df_multi , title= "Sales", x_col= "Month", y_cols =["Sales2021", "Sales2022"] )
-    # print (df_multi)
-    # plotter.heatmap (df = df_heatmap, title = 'heatmap', annot = True )
-
-    # plotter.lineplot(df=df_multi, title="Sales", x_col="Month", y_cols="Sales2021")
+plotter.scatterplot(df = df_scatter , title= "scatterplot", legend = True )
+   
+plotter.barplot(df=df_multi , title= "Barplot", x_col= "Month", y_cols =["Sales2021", "Sales2022"],  legend = True )
+    #print (df_multi)
+plotter.histplot (df = df_multi,title="Histplot")
+plotter.violinplot(df=df_multi2, title="Violinplot")
+plotter.heatmap(df=df_heatmap , title= "Heatmap")
+plotter.lineplot(df=df_multi, title="Lineplot", x_col="Month", y_cols="Sales2021")
 
 
 # Vi ska göra en klass som heter något smart, typ AvausPlotter eller något liknande
@@ -231,7 +304,20 @@ if __name__ == "__main__":
 
 
 # font = ImageFont.truetype(Roboto)
+"""""
 
+def download(self, url):
+
+     req = urllib.Request( url ) 
+     content = urllib.urlopen( req )
+     data = content.read()
+     content.close()
+     return data
+
+url = 'https://fonts.google.com/download?family=Roboto'
+dl = open(url)
+data = dl.download(url)
+""" ""
 # print(font)
 
 
