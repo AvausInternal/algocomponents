2b33bd9626cec999d0d5a75ec4dc2ed99d5bcde5
Michał Łątkowski <michallatkos@gmail.com>
2023-01-23 13:09:13 +0100

    Add ID_KEY to time grouping UDF, small fixes and refactoring naming

--- infra/udf.tf
+++ infra/udf.tf
@@ -15,7 +15,7 @@ resource "google_bigquery_routine" "test_routine_terraform" {
 # various options described here: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_routine
 
 resource "google_bigquery_routine" "get_known_users" {
-  dataset_id      = var.transform_dataset_id
+  dataset_id      = var.transform_dataset
   routine_id      = "get_known_users"
   routine_type    = "PROCEDURE"
   language        = "SQL"
@@ -34,7 +34,7 @@ resource "google_bigquery_routine" "get_known_users" {
   WHERE
     (_TABLE_SUFFIX BETWEEN START_SUFFIX
       AND END_SUFFIX)
-    AND ((LOWER(ID_KEY)='user_id' AND user_id IS NOT NULL) OR (up.key=ID_KEY AND up.value.string_value IS NOT NULL))
+    AND ((LOWER(ID_KEY)='user_id' AND user_id IS NOT NULL) OR (LOWER(up.key)=LOWER(ID_KEY) AND up.value.string_value IS NOT NULL))
   EOS
   arguments {
     name      = "ID_KEY"
@@ -52,7 +52,7 @@ resource "google_bigquery_routine" "get_known_users" {
 }
 
 resource "google_bigquery_routine" "get_unknown_users" {
-  dataset_id      = var.transform_dataset_id
+  dataset_id      = var.transform_dataset
   routine_id      = "get_unknown_users"
   routine_type    = "PROCEDURE"
   language        = "SQL"
@@ -71,7 +71,7 @@ resource "google_bigquery_routine" "get_unknown_users" {
   WHERE
     (_TABLE_SUFFIX BETWEEN START_SUFFIX
       AND END_SUFFIX)
-  AND ((LOWER(ID_KEY)='user_id' AND user_id IS NULL) OR (up.key=ID_KEY AND up.value.string_value IS NULL))
+  AND ((LOWER(ID_KEY)='user_id' AND user_id IS NULL) OR (LOWER(up.key)=LOWER(ID_KEY) AND up.value.string_value IS NULL))
   EOS
   arguments {
     name      = "ID_KEY"
@@ -89,7 +89,7 @@ resource "google_bigquery_routine" "get_unknown_users" {
 }
 
 resource "google_bigquery_routine" "get_flattened_categorical_data" {
-  dataset_id      = var.transform_dataset_id
+  dataset_id      = var.transform_dataset
   routine_id      = "get_flattened_categorical_data"
   routine_type    = "PROCEDURE"
   language        = "SQL"
@@ -106,7 +106,7 @@ FROM (
   IF
     ((LOWER(ID_KEY)='user_id'
         AND user_id IS NOT NULL)
-      OR (up.key=ID_KEY
+      OR (LOWER(up.key)=LOWER(ID_KEY)
         AND up.value.string_value IS NOT NULL),'known','unknown') AS id_type,
     user_pseudo_id,
     user_id,
@@ -126,7 +126,7 @@ FROM (
   WHERE
     (_TABLE_SUFFIX BETWEEN START_SUFFIX
     AND END_SUFFIX)
-QUALIFY ROW_NUMBER() OVER(PARTITION BY IF (LOWER(ID_KEY)='user_id',user_id, IF (up.key=ID_KEY,up.value.string_value,user_pseudo_id)) ORDER BY event_timestamp DESC) = 1
+QUALIFY ROW_NUMBER() OVER(PARTITION BY IF (LOWER(ID_KEY)='user_id',user_id, IF (LOWER(up.key)=LOWER(ID_KEY),up.value.string_value,user_pseudo_id)) ORDER BY event_timestamp DESC) = 1
 ) cte
 EOS
   arguments {
@@ -145,7 +145,7 @@ EOS
 }
 
 resource "google_bigquery_routine" "get_time_grouped_page_views_per_user" {
-  dataset_id      = var.transform_dataset_id
+  dataset_id      = var.transform_dataset
   routine_id      = "get_time_grouped_page_views_per_user"
   routine_type    = "PROCEDURE"
   language        = "SQL"
@@ -154,27 +154,50 @@ resource "google_bigquery_routine" "get_time_grouped_page_views_per_user" {
     DAYS - how many days since the current timestamp you want the data to come from
     EOS
   definition_body = <<-EOS
-SELECT user_id,time_grouping AS time_grouping,COUNT(*) AS page_views FROM
-(
-SELECT user_id, event_timestamp,
-CASE
-WHEN LOWER(MODE)='day' THEN FORMAT_TIMESTAMP('%d%m%Y',TIMESTAMP_MICROS(event_timestamp))
-WHEN LOWER(MODE)='week' THEN FORMAT_TIMESTAMP('%V%Y',TIMESTAMP_MICROS(event_timestamp))
-WHEN LOWER(MODE)='month' THEN FORMAT_TIMESTAMP('%m%Y',TIMESTAMP_MICROS(event_timestamp))
-ELSE 'INCORRECT MODE SELECTED - CHOOSE FROM: (day, week, month)'
-END AS time_grouping
-FROM
+SELECT
+  id,
+  time_grouping,
+  COUNT(*) AS page_views
+FROM (
+  SELECT
+    CASE
+      WHEN LOWER(ID_KEY)='user_id' THEN user_id
+      WHEN LOWER(up.key)=LOWER(ID_KEY) THEN up.value.string_value
+    ELSE user_pseudo_id
+  END
+    AS id,
+    event_timestamp,
+    CASE
+      WHEN LOWER(MODE)='day' THEN FORMAT_TIMESTAMP('%d%m%Y',TIMESTAMP_MICROS(event_timestamp))
+      WHEN LOWER(MODE)='week' THEN FORMAT_TIMESTAMP('%V%Y',TIMESTAMP_MICROS(event_timestamp))
+      WHEN LOWER(MODE)='month' THEN FORMAT_TIMESTAMP('%m%Y',TIMESTAMP_MICROS(event_timestamp))
+    ELSE
+    'INCORRECT MODE SELECTED - CHOOSE FROM: (day, week, month)'
+  END
+    AS time_grouping
+  FROM
   `${var.ga_project_id}.${var.ga_dataset_id}.events_*`
-  WHERE event_name = 'page_view'
-    AND
-  (TIMESTAMP_MICROS(event_timestamp) BETWEEN TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL DAYS DAY) AND CURRENT_TIMESTAMP() )
-  AND (_TABLE_SUFFIX BETWEEN FORMAT_TIMESTAMP('%Y%m%d',TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL DAYS DAY)) AND FORMAT_TIMESTAMP('%Y%m%d',CURRENT_TIMESTAMP()))
-AND user_id IS NOT NULL
-)
-GROUP BY user_id,time_grouping
-ORDER BY user_id,MAX(event_timestamp)
-
+  LEFT JOIN
+  UNNEST(user_properties) AS up
+  WHERE
+    event_name = 'page_view'
+    AND (TIMESTAMP_MICROS(event_timestamp) BETWEEN TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL DAYS DAY)
+      AND CURRENT_TIMESTAMP() )
+    AND (_TABLE_SUFFIX BETWEEN FORMAT_TIMESTAMP('%Y%m%d',TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL DAYS DAY))
+      AND FORMAT_TIMESTAMP('%Y%m%d',CURRENT_TIMESTAMP()))
+    )
+WHERE id IS NOT NULL
+GROUP BY
+  id,
+  time_grouping
+ORDER BY
+  id,
+  MAX(event_timestamp)
 EOS
+    arguments {
+    name      = "ID_KEY"
+    data_type = "{\"typeKind\" :  \"STRING\"}"
+  }
   arguments {
     name      = "MODE" # either 'day', 'week' or 'month'
     data_type = "{\"typeKind\" :  \"STRING\"}"
