dfd62683a402473d93a99d99fb6585bbc9b3ed3a
Eric Hörberg <eric.horberg@avaus.se>
2023-02-17 16:29:54 +0100

    Add test for SQLPipeline

---
+++ tests/tasks/sql_pipeline/test_sql_pipeline.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+import pytest
+
+from algocomponents.adapters import LocalSqliteAdapter
+from algocomponents.tasks import SQLPipeline
+
+
+class EmptySQLPipeline(SQLPipeline):
+    """Created here to fix sql folder path"""
+
+
+class TestSqlPipeline(TestCase):
+    def test_correct_file_formatting(self):
+        pipeline = EmptySQLPipeline(
+            sql_adapter=LocalSqliteAdapter(),
+            sql_folder="good_sql",
+        )
+        result = pipeline.start()
+
+        assert result is not None
+
+    def test_bad_file_formatting(self):
+        with pytest.raises(NameError):
+            EmptySQLPipeline(
+                sql_adapter=LocalSqliteAdapter(),
+                sql_folder="bad_sql",
+            )
