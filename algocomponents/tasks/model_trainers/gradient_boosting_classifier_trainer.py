2d26cbf3de30b4a811b382f43825307922032ea3
Eric Hörberg <eric.horberg@avaus.se>
2023-04-09 15:40:15 +0200

    Add model trainers for scikit learn models.
    Three model trainers are added, that all behave the exact same way. A
    base class was created to remove code duplication.

---
+++ algocomponents/tasks/model_trainers/gradient_boosting_classifier_trainer.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from sklearn.ensemble import GradientBoostingClassifier
+
+from algocomponents.tasks import ModelTrainer
+
+
+class GradientBoostingClassifierTrainer(ModelTrainer):
+    def __init__(
+        self,
+        dataset_table: str,
+        target_label_column: str,
+        output_path: str,
+        one_hot_encoded_columns: List[str] = None,
+        excluded_columns: List[str] = None,
+        overwrite_existing_model: bool = False,
+        **kwargs
+    ):
+        super().__init__(
+            dataset_table=dataset_table,
+            target_label_column=target_label_column,
+            output_path=output_path,
+            one_hot_encoded_columns=one_hot_encoded_columns,
+            excluded_columns=excluded_columns,
+            overwrite_existing_model=overwrite_existing_model,
+            model_type="classifier",
+            model_reference=GradientBoostingClassifier(),
+            **kwargs,
+        )
---
+++ algocomponents/tasks/model_trainers/model_trainer.py
@@ -0,0 +1,92 @@
+import json
+import os
+import shutil
+from typing import List
+
+import joblib
+from sklearn.base import BaseEstimator
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+
+from algocomponents.tasks import Task
+
+
+class ModelTrainer(Task):
+    model_file = "model.joblib"
+    preprocessor_file = "preprocessor.joblib"
+    metadata_file = "meta.json"
+
+    def __init__(
+        self,
+        dataset_table: str,
+        target_label_column: str,
+        output_path: str,
+        model_type: str,
+        model_reference: BaseEstimator,  # Any scikit learn model
+        one_hot_encoded_columns: List[str] = None,
+        excluded_columns: List[str] = None,
+        overwrite_existing_model: bool = False,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.dataset_table = dataset_table
+        self.target_label_column = target_label_column
+        self.output_path = output_path
+        self.model_type = model_type
+        self.model_reference = model_reference
+        self.one_hot_encoded_columns = one_hot_encoded_columns or []
+        self.excluded_columns = excluded_columns or []
+        self.overwrite_existing_model = overwrite_existing_model
+
+    def run(self):
+        model, preprocessor = self._create_model()
+
+        self._save_model(model, preprocessor)
+
+    def _create_model(self):
+        df = self.sql_adapter.table_as_pandas_df(self.dataset_table)
+        df = df.drop(columns=self.excluded_columns)
+
+        preprocessor = ColumnTransformer(
+            transformers=[("cat", OneHotEncoder(), self.one_hot_encoded_columns)],
+            remainder="passthrough",
+        )
+        model = Pipeline(
+            steps=[
+                ("preprocessor", preprocessor),
+                (self.model_type, self.model_reference),
+            ]
+        )
+
+        model.fit(
+            X=df.drop(columns=self.target_label_column),
+            y=df[self.target_label_column],
+        )
+
+        return model, preprocessor
+
+    def _save_model(self, model, preprocessor):
+        if self.overwrite_existing_model:
+            if os.path.exists(self.output_path):
+                if os.path.isfile(self.output_path):
+                    os.remove(self.output_path)
+                else:
+                    shutil.rmtree(self.output_path)
+        os.mkdir(self.output_path)
+
+        joblib.dump(
+            value=model, filename=os.path.join(self.output_path, self.model_file)
+        )
+        joblib.dump(
+            value=preprocessor,
+            filename=os.path.join(self.output_path, self.preprocessor_file),
+        )
+
+        metadata = {
+            "model_file": self.model_file,
+            "preprocessor_file": self.preprocessor_file,
+        }
+        metadata_path = os.path.join(self.output_path, self.metadata_file)
+        with open(metadata_path, "w+") as f:
+            json.dump(metadata, f)
