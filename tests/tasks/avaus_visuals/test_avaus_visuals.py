b7fc85d5d87f22ba8c998e59309af7f41b5b2354
Eric Hörberg <eric.horberg@avaus.se>
2023-04-12 10:12:06 +0200

    Add tests for AvausVisuals

---
+++ tests/tasks/avaus_visuals/test_avaus_visuals.py
@@ -0,0 +1,46 @@
+from unittest import TestCase
+import pandas as pd
+import os.path
+
+from algocomponents.tasks import VisualizeDataset
+from algocomponents.adapters import LocalSqliteAdapter
+from algocomponents.tasks import AvausVisuals
+
+
+class TestAvausVisuals(TestCase):
+    """Test AvausVisuals class"""
+
+    output_path = os.path.join("tests", "tasks", "avaus_visuals")
+    df_to_plot = pd.DataFrame(
+        data={
+            "x": [0],
+            "y": [0],
+        }
+    )
+    plotter = AvausVisuals()
+
+    def test_saving_a_boxplot(self):
+        self.plotter.boxplot(
+            df=self.df_to_plot,
+            x_col="x",
+            y_col="y",
+            show=False,
+            file_name="test_saving_a_boxplot",
+            output_folder=self.output_path,
+        )
+        output_file = os.path.join(self.output_path, "test_saving_a_boxplot.png")
+        assert os.path.isfile(output_file)
+        os.remove(output_file)
+
+    def test_saving_a_countplot(self):
+        self.plotter.countplot(
+            df=self.df_to_plot,
+            x_col="x",
+            y_col="y",
+            show=False,
+            file_name="test_saving_a_countplot",
+            output_folder=self.output_path,
+        )
+        output_file = os.path.join(self.output_path, "test_saving_a_countplot.png")
+        assert os.path.isfile(output_file)
+        os.remove(output_file)
