9d5adbb1276f569d4a219a503bf89af2330634de
Eric Hörberg <eric.horberg@avaus.se>
2023-04-24 08:45:33 +0200

    Refactor VisualizeDataset.
    You can now manually supply the columns you are interested in using. If
    you do not, you can instead use the previous logic by seting the flag
    find_feature_columns to True.

--- algocomponents/tasks/_visualize_dataset.py
+++ algocomponents/tasks/_visualize_dataset.py
@@ -1,6 +1,6 @@
-import pandas as pd
-import numpy as np
+from typing import List
 
+import pandas as pd
 
 from algocomponents.tasks import Task
 from algocomponents.utils import save_boxplot, save_histogram, save_corr_matrix
@@ -14,10 +14,16 @@ class VisualizeDataset(Task):
     boxplot, normalized boxplot, histograms, correlation matrix of that dataset.
 
     Attributes:
-        input_df (pd.DataFrame, optional): Pandas DataFrame with dataset to visualize.
-        input_csv_file (str, optional): Csv file path with dataset to visualize.
-        input_table_name (str, optional): Database table name with dataset to visualize
+        input_df: Pandas DataFrame with dataset to visualize.
+        input_csv_file: Csv file path with dataset to visualize.
+        input_table_name: Database table name with dataset to visualize
             following "{DATASET}.{TABLE}" naming convention.
+        continuous_features: A list of all continuous features to plot
+        categorical_features: A list of all categorical features to plot
+        excluded_columns: A list of columns to not plot
+        find_feature_columns: A boolean flag indicating whether this task should
+            attempt to find the columns on its own. Cannot be set to True if
+            either continuous_features or categorical_features are set
         output_folder (str, optional): Folder name or folder path, where plots are to be saved.
         interactive_plots (bool, optional): Whether to additionally save interactive plots.
 
@@ -28,21 +34,25 @@ class VisualizeDataset(Task):
         input_df: pd.DataFrame = None,
         input_csv_file: str = None,
         input_table_name: str = None,
+        continuous_features: List[str] = None,
+        categorical_features: List[str] = None,
+        excluded_columns: List[str] = None,
+        find_feature_columns: bool = False,
         output_folder: str = "",
         interactive_plots: bool = True,
         **kwargs,
     ):
-        super().__init__(**kwargs)
-        self.input_df = input_df
-        self.input_csv_file = input_csv_file
-        self.input_table_name = input_table_name
-        self.output_folder = output_folder
-        self.interactive_plots = interactive_plots
+        if find_feature_columns:
+            if continuous_features or categorical_features:
+                raise ValueError(
+                    "When running with find_feature_columns as True,"
+                    "you cannot specify continuous_features or categorical_features."
+                )
 
         only_one_true_list = [
-            self.input_csv_file is not None,
-            self.input_table_name is not None,
-            self.input_df is not None,
+            input_csv_file is not None,
+            input_table_name is not None,
+            input_df is not None,
         ]
         if only_one_true_list.count(True) > 1:
             raise ValueError(
@@ -55,11 +65,22 @@ class VisualizeDataset(Task):
                 "got none."
             )
 
-        if self.input_table_name and self.sql_adapter is None:
+        if input_df is not None and input_df.empty:
+            raise ValueError("Input_df is empty.")
+
+        super().__init__(**kwargs)
+        if input_table_name and self.sql_adapter is None:
             raise ValueError("You must provide sql_adapter with input_table_name.")
 
-        if self.input_df is not None and self.input_df.empty:
-            raise ValueError("Input_df is empty.")
+        self.input_df = input_df
+        self.input_csv_file = input_csv_file
+        self.input_table_name = input_table_name
+        self.continuous_features = continuous_features or []
+        self.categorical_features = categorical_features or []
+        self.excluded_columns = excluded_columns or []
+        self.output_folder = output_folder
+        self.interactive_plots = interactive_plots
+        self.find_feature_columns = find_feature_columns
 
     def run(self):
         if self.input_csv_file:
@@ -67,8 +88,11 @@ class VisualizeDataset(Task):
         elif self.input_table_name:
             self.input_df = self.sql_adapter.table_as_pandas_df(self.input_table_name)
 
-        # dataframe with only numerical features
-        numerics = ["int16", "int32", "int64", "float16", "float32", "float64"]
+        if self.find_feature_columns:
+            numerics = ["int16", "int32", "int64", "float16", "float32", "float64"]
+        else:
+            numerics = self.continuous_features
+
         self.df_numeric = self.input_df.select_dtypes(include=numerics)
         # dataframe with only numerical features and normalized values
         self.df_normalized = (self.df_numeric - self.df_numeric.min()) / (
