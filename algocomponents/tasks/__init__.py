fe5633e57ab07421d01b6f6537499e78f6638d9e
michal.latkowski <95039431+michal-latkowski-avaus@users.noreply.github.com>
2023-05-16 19:59:53 +0200

    Add DataTransferTask to tsks __init__.py

--- algocomponents/tasks/__init__.py
+++ algocomponents/tasks/__init__.py
@@ -25,6 +25,7 @@ from algocomponents.tasks.model_trainers.gradient_boosting_classifier_trainer im
 from algocomponents.tasks._visualize_features import (
     VisualizeFeatures,
 )
+from algocomponents.tasks._data_transfer_task import DataTransferTask
 
 """Allows classes to live in separate files while keeping imports short
 
@@ -52,4 +53,5 @@ __all__ = [
     "RandomForestClassifierTrainer",
     "GradientBoostingClassifierTrainer",
     "VisualizeFeatures",
+    "DataTransferTask",
 ]
