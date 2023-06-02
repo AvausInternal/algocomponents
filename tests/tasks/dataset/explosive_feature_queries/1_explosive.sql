a688b80442d0a1c80b50f91a90694d6f19fd8184
Martti Yap <martti.yap@avaus.com>
2023-06-02 16:04:45 +0200

    additional tests in place

---
+++ tests/tasks/dataset/explosive_feature_queries/1_explosive.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS {output_table};
+
+CREATE TABLE {output_table} AS
+SELECT
+    user_id,
+    COUNT(product_id) AS n_products_bought,
+    COUNT(DISTINCT product_id) AS n_distinct_products_bought
+FROM {input_table}
+GROUP BY user_id;
+
+-- userid 1 is duplicated 
+INSERT INTO {output_table} 
+VALUES (1, 1, 1);
