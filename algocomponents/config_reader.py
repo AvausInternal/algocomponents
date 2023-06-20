182413ea613613c5c9981a4c3f17bb1b886571a1
Eric Hörberg <eric.horberg@avaus.se>
2023-06-20 12:43:36 +0200

    Move format_query to format_string in ConfigReader

--- algocomponents/config_reader.py
+++ algocomponents/config_reader.py
@@ -5,6 +5,7 @@ from abc import ABC
 from configparser import ConfigParser
 from datetime import datetime
 from types import ModuleType
+from typing import Dict
 
 from algocomponents.utils import LoggieDoggie, config_to_str, merge_configs
 
@@ -130,3 +131,53 @@ class ConfigReader(ABC):
             logger_name=self.class_name,
             config=dict(self.config[self.section]),
         )
+
+    def format_string(
+        self,
+        string: str,
+        additional_format_variables: Dict[str, str] = None,
+        max_depth: int = 5,
+    ) -> str:
+        """Recursively .format():s a string given a dict until it does not change.
+
+        A max depth is used, as writing a more general approach to this method
+        involves solving self-referencing problems in the format variables. For
+        example, {"a": "{b}", "b": "{a}"} which will cause "{a}" to be formatted
+        into "{b}", which will format into "{a}", etc. There are solutions, but
+        the added code complexity was deemed to not be worth it.
+
+        Args:
+            string: The string to format.
+            additional_format_variables: A dictionary used to .format() the SQL string.
+            max_depth: Max number of times .format() will be done.
+
+        Returns:
+            The provided string, formatted.
+
+        Raises:
+            RecursionError: When .format():ing more than max_depth times and the
+                string is still changing
+
+        Examples:
+            {output_table} -> {tmp_db}.output_table -> tmp.output_table
+
+        """
+        if not additional_format_variables:
+            additional_format_variables = {}
+
+        format_variables = dict(self.config[self.section]).copy()
+        format_variables.update(additional_format_variables)
+
+        previous_string = ""
+        depth = 0
+        while string != previous_string:
+            previous_string = string
+            string = string.format(**format_variables)
+            depth += 1
+            if depth > max_depth:
+                raise RecursionError(
+                    f"Reached max reformatting depth of {max_depth} with:\n"
+                    f"string:\n{string}\n"
+                    f"previous_string:\n{previous_string}"
+                )
+        return string
