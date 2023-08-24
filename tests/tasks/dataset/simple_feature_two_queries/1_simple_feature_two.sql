8badbf1451a64b76eeb10055275d8a13412795b6
Martti Yap <martti.yap@avaus.com>
2023-08-24 15:29:27 +0200

    add 4 space indenting

--- tests/tasks/dataset/simple_feature_two_queries/1_simple_feature_two.sql
+++ tests/tasks/dataset/simple_feature_two_queries/1_simple_feature_two.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS {output_table};
 
 CREATE TABLE {output_table} AS
 SELECT 
-  product_id,
-  product_price,
-  product_weight,
-  product_price*1.0/product_weight as price_per_kg
+    product_id,
+    product_price,
+    product_weight,
+    product_price*1.0/product_weight as price_per_kg
 FROM
-  {input_table}
+    {input_table}
 ;
