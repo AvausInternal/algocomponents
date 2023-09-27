b5430ac181bb51a30aeaa321ad3f942855739613
Eric Hörberg <eric.horberg@avaus.se>
2023-09-27 15:26:03 +0200

    Add tests related to predicting to csv

--- tests/tasks/predict/test_predict.py
+++ tests/tasks/predict/test_predict.py
@@ -60,6 +60,15 @@ class TestPredict(TestCase):
                 overwrite_output=True,
             ).start()
 
+    def test_predicting_without_output(self):
+        with pytest.raises(ValueError):
+            Predict(
+                sql_adapter=self.sql_adapter,
+                dataset_table=self.double_input_table,
+                target_label_column=self.target_label_column,
+                model_path=self.model_path,
+            ).start()
+
     def test_predicting_with_bad_metadata_file(self):
         if not os.path.exists(self.model_path):
             os.makedirs(self.model_path)
@@ -95,7 +104,25 @@ class TestPredict(TestCase):
 
         shutil.rmtree(self.model_path)
 
-    def test_predicting_with_real_model(self):
+    def test_predicting_to_csv_that_exists(self):
+        self.create_real_model()
+        csv_file_path = os.path.join(self.model_path, "model_output.csv")
+
+        with open(csv_file_path, "w") as file:
+            file.write("Nothing")
+
+        with pytest.raises(ValueError):
+            Predict(
+                sql_adapter=self.sql_adapter,
+                dataset_table=self.double_input_table,
+                target_label_column=self.target_label_column,
+                model_path=self.model_path,
+                output_prediction_csv=csv_file_path,
+            ).start()
+
+        shutil.rmtree(self.model_path)
+
+    def test_predicting_with_real_model_to_table(self):
         self.create_real_model()
 
         Predict(
@@ -118,3 +145,57 @@ class TestPredict(TestCase):
         self.sql_adapter.run_sql_string(f"DROP TABLE {self.output_table}")
         shutil.rmtree(self.model_path)
         self.sql_adapter.disconnect()
+
+    def test_predicting_with_real_model_to_csv(self):
+        self.create_real_model()
+        csv_file_path = os.path.join(self.model_path, "model_output.csv")
+
+        Predict(
+            sql_adapter=self.sql_adapter,
+            dataset_table=self.double_input_table,
+            target_label_column=self.target_label_column,
+            model_path=self.model_path,
+            output_prediction_csv=csv_file_path,
+            overwrite_output=True,
+        ).start()
+
+        assert os.path.isfile(csv_file_path)
+
+        with open(csv_file_path, "r") as file:
+            csv_headers = file.readline()
+        assert "score" in csv_headers
+
+        shutil.rmtree(self.model_path)
+
+    def test_predicting_with_real_model_to_table_and_csv(self):
+        self.create_real_model()
+        csv_file_path = os.path.join(self.model_path, "model_output.csv")
+
+        Predict(
+            sql_adapter=self.sql_adapter,
+            dataset_table=self.double_input_table,
+            target_label_column=self.target_label_column,
+            model_path=self.model_path,
+            output_prediction_table=self.output_table,
+            output_prediction_csv=csv_file_path,
+            overwrite_output=True,
+        ).start()
+
+        self.sql_adapter.connect()
+        assert self.sql_adapter.table_exists(table=self.output_table)
+        assert self.sql_adapter.table_contains_columns(
+            table=self.output_table,
+            columns=["score"],
+        )
+        assert self.sql_adapter.count_rows_in_table(table=self.output_table) > 0
+
+        self.sql_adapter.run_sql_string(f"DROP TABLE {self.output_table}")
+        self.sql_adapter.disconnect()
+
+        assert os.path.isfile(csv_file_path)
+
+        with open(csv_file_path, "r") as file:
+            csv_headers = file.readline()
+        assert "score" in csv_headers
+
+        shutil.rmtree(self.model_path)
