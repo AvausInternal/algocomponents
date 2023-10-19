c22fa95053dcc4fc48162f51c3e4e864b088687b
Michał Łątkowski <michallatkos@gmail.com>
2023-10-19 12:56:43 +0200

    Code review and formatting

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -1,8 +1,19 @@
+from configparser import ConfigParser
+
 import numpy as np
 import pandas as pd
 import pytest
-from configparser import ConfigParser
-from algocomponents.utils import config_to_str, predict_with_model
+
+from algocomponents.utils import config_to_str
+from algocomponents.utils import predict_with_model
+
+
+class FakeModel:
+    def predict(self, df):
+        return [1 for _ in range(len(df))]
+
+    def predict_proba(self, df):
+        return np.array([[0.1, 0.9] for _ in range(len(df))])
 
 
 class TestUtils:
@@ -27,39 +38,38 @@ class TestUtils:
         config.set(section="DEVIANT", option="p", value="q")
         config_str = config_to_str(config)
         assert (
-                config_str
-                == "{'DEFAULT': {'a': 'b', 'x': 'y'}, 'DEVIANT': {'a': 'o', 'p': 'q', 'x': 'y'}}"
+            config_str
+            == "{'DEFAULT': {'a': 'b', 'x': 'y'}, 'DEVIANT': {'a': 'o', 'p': 'q', 'x': 'y'}}"
         )
-
-    class FakeModel:
-        def predict(self, df):
-            return [1 for _ in range(len(df))]
-
-        def predict_proba(self, df):
-            return np.array([[0.1, 0.9] for _ in range(len(df))])
 
     @pytest.fixture
     def sample_data(self):
         return {
-            "metadata": {
-                "model_file": "model.pkl"
-            },
+            "metadata": {"model_file": "model.pkl"},
             "df": pd.DataFrame({"feature1": [1, 2, 3], "feature2": [4, 5, 6]}),
-            "model_path": "/path/to/model"
+            "model_path": "/path/to/model",
         }
 
     @pytest.fixture(autouse=True)
     def mock_model(self, monkeypatch):
-        monkeypatch.setattr("joblib.load", lambda filename: TestUtils.FakeModel())
+        monkeypatch.setattr("joblib.load", lambda filename: FakeModel())
 
     def test_predict_with_probabilities(self, sample_data):
-        result_df = predict_with_model(sample_data["model_path"], sample_data["metadata"], sample_data["df"].copy(),
-                                       predict_probabilities=True)
+        result_df = predict_with_model(
+            sample_data["model_path"],
+            sample_data["metadata"],
+            sample_data["df"].copy(),
+            predict_probabilities=True,
+        )
         assert "score" in result_df.columns
         assert result_df["score"].tolist() == [0.9, 0.9, 0.9]
 
     def test_predict_without_probabilities(self, sample_data):
-        result_df = predict_with_model(sample_data["model_path"], sample_data["metadata"], sample_data["df"].copy(),
-                                       predict_probabilities=False)
+        result_df = predict_with_model(
+            sample_data["model_path"],
+            sample_data["metadata"],
+            sample_data["df"].copy(),
+            predict_probabilities=False,
+        )
         assert "score" in result_df.columns
         assert result_df["score"].tolist() == [1, 1, 1]
