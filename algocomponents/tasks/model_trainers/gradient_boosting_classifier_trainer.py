6da8b1115bc0b427f26dfa7cc17870e598a397e6
michal-latkowski-avaus <michallatkos@gmail.com>
2023-12-14 09:35:30 +0100

    Rename classifier to classification in models

--- algocomponents/tasks/model_trainers/gradient_boosting_classifier_trainer.py
+++ algocomponents/tasks/model_trainers/gradient_boosting_classifier_trainer.py
@@ -39,7 +39,7 @@ class GradientBoostingClassifierTrainer(ModelTrainer):
             categorical_columns=categorical_columns,
             excluded_columns=excluded_columns,
             overwrite_existing_model=overwrite_existing_model,
-            model_type="classifier",
+            model_type="classification",
             model_reference=GradientBoostingClassifier(),
             **kwargs,
         )
