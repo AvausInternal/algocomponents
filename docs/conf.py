317cb49be3cacb3af4215df1b2e136ee3ca172f7
Eric Hörberg <eric.horberg@avaus.se>
2024-11-11 15:49:58 +0100

    Set version number to 1.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "Algocomponents"
 copyright = "2024, Avaus"
 author = "Avaus"
-release = "1.0.1"
+release = "1.0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
