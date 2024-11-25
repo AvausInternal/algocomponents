7f29e4a7a1b1752756246dc883e9f192d58e51e1
Eric Hörberg <eric.horberg@avaus.se>
2024-11-25 14:49:59 +0100

    Set version number to 1.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def load_requirements(path):
 
 setup(
     name="algocomponents",
-    version="1.0.2",
+    version="1.0.3",
     packages=[
         "algocomponents",
         "algocomponents.tasks",
