e94713a18fddffd77019c3752724b9e6c7bb4381
Eric Hörberg <eric.horberg@avaus.se>
2025-11-26 14:13:27 +0100

    Rename alias to non-keyword term

--- tests/adapters/big_query_adapter_format_queries/update_set_query.sql
+++ tests/adapters/big_query_adapter_format_queries/update_set_query.sql
@@ -1,58 +1,58 @@
-MERGE `mocked_data.profile_component` AS target
-USING `tmp.mock_model_output` AS source
-    ON target.profileId = source.profileId
+MERGE `mocked_data.profile_component` AS tgt
+USING `tmp.mock_model_output` AS src
+    ON tgt.profileId = src.profileId
 WHEN MATCHED THEN
     -- We cannot update one value in struct in BigQuery,
     -- so we need to replace the entire struct
     UPDATE SET
         behavioral = STRUCT(
             STRUCT(
-                target.behavioral.commercial.firstPurchaseUtc
-                , target.behavioral.commercial.lastPurchaseUtc
-                , target.behavioral.commercial.orderAverageValue
-                , target.behavioral.commercial.orderCount
-                , target.behavioral.commercial.orderMedianValue
-                , target.behavioral.commercial.orderTotalValue
+                tgt.behavioral.commercial.firstPurchaseUtc
+                , tgt.behavioral.commercial.lastPurchaseUtc
+                , tgt.behavioral.commercial.orderAverageValue
+                , tgt.behavioral.commercial.orderCount
+                , tgt.behavioral.commercial.orderMedianValue
+                , tgt.behavioral.commercial.orderTotalValue
             )
                 AS commercial
                 -- This is where the ltv_score is inserted
-            , source.ltv_score AS customerLifetimeValue
-            , target.behavioral.firstSeen
-            , target.behavioral.lastSeen
-            , target.behavioral.sessionAverageTimeInSeconds
-            , target.behavioral.sessionCount
-            , target.behavioral.subscriptionCountDoubleOptIn
-            , target.behavioral.subscriptionCountSingleOptIn
-            , target.behavioral.totalTimeSpentOnSiteInMilliseconds
-            , target.behavioral.unsubscribeAll
-            , target.behavioral.unsubscribeCount
+            , src.ltv_score AS customerLifetimeValue
+            , tgt.behavioral.firstSeen
+            , tgt.behavioral.lastSeen
+            , tgt.behavioral.sessionAverageTimeInSeconds
+            , tgt.behavioral.sessionCount
+            , tgt.behavioral.subscriptionCountDoubleOptIn
+            , tgt.behavioral.subscriptionCountSingleOptIn
+            , tgt.behavioral.totalTimeSpentOnSiteInMilliseconds
+            , tgt.behavioral.unsubscribeAll
+            , tgt.behavioral.unsubscribeCount
         );
-MERGE `avaus-academy.mocked_data.profile_component` AS target
-USING `avaus-academy.tmp.mock_model_output` AS source
-    ON target.profileId = source.profileId
+MERGE `avaus-academy.mocked_data.profile_component` AS tgt
+USING `avaus-academy.tmp.mock_model_output` AS src
+    ON tgt.profileId = src.profileId
 WHEN MATCHED THEN
     -- We cannot update one value in struct in BigQuery,
     -- so we need to replace the entire struct
     UPDATE SET
         behavioral = STRUCT(
             STRUCT(
-                target.behavioral.commercial.firstPurchaseUtc
-                , target.behavioral.commercial.lastPurchaseUtc
-                , target.behavioral.commercial.orderAverageValue
-                , target.behavioral.commercial.orderCount
-                , target.behavioral.commercial.orderMedianValue
-                , target.behavioral.commercial.orderTotalValue
+                tgt.behavioral.commercial.firstPurchaseUtc
+                , tgt.behavioral.commercial.lastPurchaseUtc
+                , tgt.behavioral.commercial.orderAverageValue
+                , tgt.behavioral.commercial.orderCount
+                , tgt.behavioral.commercial.orderMedianValue
+                , tgt.behavioral.commercial.orderTotalValue
             )
                 AS commercial
                 -- This is where the ltv_score is inserted
-            , source.ltv_score AS customerLifetimeValue
-            , target.behavioral.firstSeen
-            , target.behavioral.lastSeen
-            , target.behavioral.sessionAverageTimeInSeconds
-            , target.behavioral.sessionCount
-            , target.behavioral.subscriptionCountDoubleOptIn
-            , target.behavioral.subscriptionCountSingleOptIn
-            , target.behavioral.totalTimeSpentOnSiteInMilliseconds
-            , target.behavioral.unsubscribeAll
-            , target.behavioral.unsubscribeCount
+            , src.ltv_score AS customerLifetimeValue
+            , tgt.behavioral.firstSeen
+            , tgt.behavioral.lastSeen
+            , tgt.behavioral.sessionAverageTimeInSeconds
+            , tgt.behavioral.sessionCount
+            , tgt.behavioral.subscriptionCountDoubleOptIn
+            , tgt.behavioral.subscriptionCountSingleOptIn
+            , tgt.behavioral.totalTimeSpentOnSiteInMilliseconds
+            , tgt.behavioral.unsubscribeAll
+            , tgt.behavioral.unsubscribeCount
         )
