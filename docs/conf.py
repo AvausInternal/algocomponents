8a57aca18add3f09d47a4507add98b04cbd7b06b
Eric Hörberg <eric.horberg@avaus.se>
2025-11-27 09:07:05 +0100

    Set copyright year to 2025

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "Algocomponents"
-copyright = "2024, Avaus"
+copyright = "2025, Avaus"
 author = "Avaus"
 release = "1.0.5"
 
